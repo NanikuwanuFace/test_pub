70b0b3db8a67f40924012132342e89c4a1753ff9
ryou ikuta <ryou-ikuta@tas-japan.com>
2021-10-04 15:04:01 +0900

    dady01の更新

--- R/dady01.R
+++ R/dady01.R
@@ -1,11 +1,32 @@
-# 01_不確実性
+# 01_不確実性####
 
-#作業環境の変更
+#作業環境の変更####
 getwd()
-setwd("D:/github/loacal_test_repo/R")
+setwd("D:/github/loacal_test_repo/R/BDS/examples")
 
-# データインポート
+# データインポート####
 trucks <- read.csv("pickup.csv")
 head(trucks)
 
+browser <- read.csv("web-browsers.csv")
+dim(browser)
+head(browser)
 ###################################################
+# 平均####
+mean(browser$spend)
+hist(browser$spend, breaks = 1000,xlim=c(0,20000))
+
+# 標本分散####
+br_n = nrow(browser)
+var(browser$spend)/br_n
+
+# ノンパラメトリックブートストラップ####
+B <- 1000 # ブートストラップ回数
+mub <- c() # 標本平均ベクトル
+for (b in 1:B){
+  samp_b <- sample.int((nrow(browser)), replace=TRUE)
+  mub <- c(mub, mean(browser$spend[samp_b]))
+  }
+sd (mub)
+
+sort(samp_b)[1:10]
